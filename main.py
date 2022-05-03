36410d0048fcea8a3c10b9757cb22b347ad0a6cb
Robot1235369 <IsaacCA2018@gmail.com>
2022-05-03 18:14:20 +0000

    stuff

---
+++ main.py
@@ -0,0 +1,11 @@
+isjump = False
+velocity = 0
+jumpheight = 10
+y = 0
+
+def jump():
+    global isjump
+    global velocity
+    if not isjump:
+        for i in range(jumpheight):
+            pass
