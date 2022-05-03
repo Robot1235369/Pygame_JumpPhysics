5de6b6deb42d9031c4ac32ae68342076a5c5b1f1
Robot1235369 <IsaacCA2018@gmail.com>
2022-05-03 19:14:36 +0000

    stuff

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 isjump = False
-velocity = 0
-jumpheight = 10
+velocity = 25
+jumpheight = 50
 y = 0
 
 def jump():
     global isjump
     global velocity
+    global y
     if not isjump:
         for i in range(jumpheight):
-            pass
+            y += velocity
+            velocity -= 1
+            print(y)
+
+print(y)
+jump()
+print(y)
