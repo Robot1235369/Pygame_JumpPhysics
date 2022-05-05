640484708c4d66cd9940ec8fbeb0cf5f870b94a6
Robot1235369 <IsaacCA2018@gmail.com>
2022-05-05 17:43:05 +0000

    stuff

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 velocity = 0
 acceleration = 0.25
 y = 0
+jumpheight = 60
 isjump = False
+
+def fall():
+    global isjump
+    global y
+    global velocity
+    global acceleration
+    global jumpheight
+    velocity += acceleration
+    y -= velocity
+    print(y)
 
 def main():
     global isjump
     global y
+    global velocity
+    global acceleration
+    global jumpheight
+    i = 0
     while True:
+        if i == 30:
+            break
         if isjump == False:
             isjump = True
-            velocity = 
+            velocity = 0
 
         if isjump == True:
-            velocity += acceleration
-            if v < 0:
-                m = -1
-            if v == -6:
-                isjump = False
-                v = 5
-                m = 1
-                print(y)
-                break
+            fall()
+            #if velocity == jumpheight:
+                #fall()
+            #if == -6:
+                #isjump = False
+                #v = 5
+                #m = 1
+                #print(y)
+                #break
+        i += 1
 
 if __name__ == "__main__":
     main()
