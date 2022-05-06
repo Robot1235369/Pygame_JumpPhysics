7f274aed36df5de33ef049214d92b69d140936c1
Robot1235369 <IsaacCA2018@gmail.com>
2022-05-06 17:30:38 +0000

    stuff

--- main.py
+++ main.py
@@ -2,19 +2,79 @@ import pygame
 import sys
 pygame.init()
 
+display = pygame.display.set_mode((500, 500))
+
 FPS = 60
 running = True
 
+hangtime = 60
+jumpheight = 60
+gravity = jumpheight / ((hangtime**2) / 2)
+velocity = gravity * (hangtime / 2)
+isjump = False
+
+x = 225
+y = 450
+pygame.draw.rect(display, (255, 0, 0), (x, y, 50, 50))
+
+def create_vars():
+    global hangtime
+    global jumpheight
+    global gravity
+    global velocity
+    hangtime = 60
+    jumpheight = 60
+    gravity = jumpheight / ((hangtime**2) / 2)
+    velocity = gravity * (hangtime / 2)
+
+def fall():
+    global y
+    global isjump
+    global gravity
+    global velocity
+    global jumpheight
+    global hangtime
+    if isjump == False:
+        if y + velocity < 450:
+            y += velocity
+            velocity += gravity
+        else:
+            y = 450
+
+
 def update():
+    global x
+    global y
+    display.fill(0, 0, 0)
+    pygame.draw.rect(display, (255, 0, 0), (x, y, 50, 50))
     pygame.display.update()
 
 def main():
+    global x
+    global y
+    global isjump
+    global gravity
+    global velocity
+    global jumpheight
+    global hangtime
+    global running
+    global FPS
     clock = pygame.Clock()
     while running:
         clock.tick(FPS)
         for event in pygame.event.get():
-            if event.type == pygame.EXIT:
+            if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                pass
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_SPACE] and isjump == False:
+            isjump = True
+            starty = y
+        if isjump == True:
+            if y - velocity == starty + 60:
+                y -= velocity
+                velocity += gravity
+
+        fall()
+        update()
     pygame.quit()
