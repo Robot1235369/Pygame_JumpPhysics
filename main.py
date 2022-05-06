ec88a49f195707b4264f73a542a4287adee3c3ae
Robot1235369 <IsaacCA2018@gmail.com>
2022-05-06 15:36:39 +0000

    stuff

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import pygame
 import sys
+pygame.init()
 
+FPS = 60
+running = True
+
+def update():
+    pygame.display.update()
+
+def main():
+    clock = pygame.Clock()
+    while running:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.EXIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                pass
+    pygame.quit()
